5177fcdcc31b1b47d77bd6fb060fdbaec732793f
mitsuoysharag <mitsuoysharag@gmail.com>
2020-05-01 21:13:23 -0500

    deploy project v2

--- deploy-page.sh
+++ deploy-page.sh
@@ -2,5 +2,5 @@ npm run build
 cd dist
 git init
 git add -A
-git commit -m 'deploy page'
+git commit -m 'deploy page v2'
 git push -f https://github.com/mitsuoysharag/admision-virtual.git  master:gh-pages
--- deploy-project.sh
+++ deploy-project.sh
@@ -1,4 +1,4 @@
 # git remote add origin https://github.com/mitsuoysharag/admision-virtual.git
 git add .
-git commit -m 'deploy project'
+git commit -m 'deploy project v2'
 git push origin master
