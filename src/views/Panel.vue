73810c90a24eb9a66a068f3db30fdc2d3bc0d4b4
mitsuoysharag <mitsuoysharag@gmail.com>
2020-05-01 22:03:45 -0500

    deploy project v2

--- src/views/Panel.vue
+++ src/views/Panel.vue
@@ -1,36 +1,135 @@
 <template>
-  <AppSidebar :links="links">
-    <Profile :slot="0" />
-    <Exam :slot="1" />
-  </AppSidebar>
+  <div>
+    <Header />
+    <Loading :active="loading" />
+    <!-- PROFILE -->
+    <section class="profile card">
+      <p class="profile__welcome">Bienvenido,</p>
+      <p
+        class="profile__name"
+      >{{profile.apellido_paterno}} {{profile.apellido_materno}} {{profile.nombre}}</p>
+      <section class="profile__content">
+        <!--  -->
+        <i class="fa fa-id-card"></i>
+        <span class="profile__item">DNI:</span>
+        <span class="profile__value">{{profile.dni}}</span>
+        <!--  -->
+        <i class="fa fa-graduation-cap"></i>
+        <span class="profile__item">Cód. Postulante:</span>
+        <span class="profile__value">{{profile.codigo_postulante}}</span>
+      </section>
+    </section>
+    <!-- EXAM -->
+    <section class="profile card">
+      <section class="profile__content">
+        <!--  -->
+        <i class="fa fa-question"></i>
+        <span class="profile__item">N° Preguntas:</span>
+        <span class="profile__value">{{examen.tamaño}}</span>
+        <!--  -->
+        <i class="fas fa-clock"></i>
+        <span class="profile__item">Inicio:</span>
+        <span class="profile__value">{{examen.tiempo_inicio}}</span>
+        <!--  -->
+        <i class="fas fa-clock"></i>
+        <span class="profile__item">Fin:</span>
+        <span class="profile__value">{{examen.tiempo_fin}}</span>
+      </section>
+      <div class="profile__actions card__actions" @click="select()">
+        <button class="button button--blue">Ingresar</button>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import AppSidebar from "@/components/AppSidebar";
-import Profile from "@/components/Panel/Profile";
-import Exam from "@/components/Panel/Exam";
+import Header from "@/components/Header";
+import Loading from "@/components/Loading";
+
+import { obtenerPerfil } from "@/services/postulanteService";
+import { obtenerExamenDatos } from "@/services/examenService";
+import { redirect } from "@/services/router";
 
 export default {
   data: () => ({
-    // link: "profile"
-    links: [
-      {
-        icon: "fa fa-user",
-        text: "Mis Datos"
-      },
-      {
-        icon: "fa fa-tasks",
-        text: "Examen Virtual"
-      }
-    ]
+    examen: {},
+    profile: {},
+    loading: true
   }),
+  async mounted() {
+    this.profile = await obtenerPerfil();
+    this.examen = await obtenerExamenDatos();
+    if (this.examen) {
+      this.examen.tiempo_inicio = this.dateFormat(this.examen.tiempo_inicio);
+      this.examen.tiempo_fin = this.dateFormat(this.examen.tiempo_fin);
+    }
+    this.loading = false;
+  },
+  methods: {
+    select() {
+      redirect("examen");
+    },
+    dateFormat(date) {
+      date = new Date(date);
+      let day = date.getDate();
+      let month = this.format_two_digits(date.getMonth() + 1);
+      let year = date.getFullYear();
+      let hours = this.format_two_digits(date.getHours());
+      let minutes = this.format_two_digits(date.getMinutes());
+      date = `${day}-${month}-${year} ${hours}:${minutes}`;
+      return date;
+    },
+    format_two_digits(n) {
+      return n < 10 ? "0" + n : n;
+    }
+  },
   components: {
-    AppSidebar,
-    Profile,
-    Exam
+    Header,
+    Loading
   }
 };
 </script>
 
 <style lang='scss' scoped>
+.profile {
+  max-width: 540px;
+  margin: 20px auto;
+  padding: 28px;
+  color: #313435;
+
+  &__welcome {
+    margin: 0 0 4px 0;
+    // color: #a7a7a7;
+    color: #7db1f7;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+  &__name {
+    margin: 0 0 28px 0;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+  &__content {
+    //
+    display: grid;
+    grid-template-columns: auto auto 1fr;
+    row-gap: 18px;
+    column-gap: 8px;
+    align-items: center;
+  }
+  &__item {
+    margin-right: 18px;
+    font-size: 0.95rem;
+    font-weight: bold;
+  }
+  &__value {
+    padding: 8px 14px;
+    background: #f6f6f6;
+    font-size: 0.95rem;
+    border-radius: 4px;
+  }
+  &__actions {
+    margin-top: 24px;
+  }
+}
 </style>
