68868c6230f9c02c1750355f2f5357ad44a49721
mitsuoysharag <mitsuoysharag@gmail.com>
2020-05-07 18:22:22 -0500

    deploy project v2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/admision-virtual/'
-//     : '/'
-// }
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/admision-virtual/'
+    : '/'
+}
